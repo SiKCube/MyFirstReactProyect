e9fb7db113b8fb5e8d7ad9d71b82575f3caf5317
SiK-1512 <ignacio.poncedeleon.6p@gmail.com>
2023-12-16 16:05:07 -0400

    My first react proyect

---
+++ my-first-react-proyect/src/Game.jsx
@@ -0,0 +1,26 @@
+export function BottomNavBar() {
+    return (
+        <nav className="bottom-nav-bar">
+            <ul>
+                <li>
+                    <a href="">
+                        <svg viewBox="0 0 21 21" width="21" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" transform="translate(1 1)"><path d="m.5 9.5 9-9 9 9" /><path d="m2.5 7.5v8c0 .5522847.44771525 1 1 1h3c.55228475 0 1-.4477153 1-1v-4c0-.5522847.44771525-1 1-1h2c.5522847 0 1 .4477153 1 1v4c0 .5522847.4477153 1 1 1h3c.5522847 0 1-.4477153 1-1v-8" /></g></svg>
+                        
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <svg viewBox="0 0 21 21" width="21" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" transform="translate(3 3)"><path d="m7 1.5h-4.5c-1.1045695 0-2 .8954305-2 2v9.0003682c0 1.1045695.8954305 2 2 2h10c1.1045695 0 2-.8954305 2-2v-4.5003682" /><path d="m14.5.46667982c.5549155.5734054.5474396 1.48588056-.0167966 2.05011677l-6.9832034 6.98320341-3 1 1-3 6.9874295-7.04563515c.5136195-.5178979 1.3296676-.55351813 1.8848509-.1045243z" /><path d="m12.5 2.5.953 1" /></g></svg>
+                        
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <svg viewBox="0 0 21 21" width="21" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" transform="translate(3 3)"><path d="m2 4.5h11c1.1045695 0 2 .8954305 2 2v6.0488218c0 1.0543618-.8158778 1.9181652-1.8507377 1.9945143l-.1567755.0054716-10.99999997-.0413227c-1.10162878-.0041384-1.99248683-.8983493-1.99248683-1.9999859v-6.0074991c0-1.1045695.8954305-2 2-2z" /><path d="m2.464 2.5h10.036" /><path d="m4.5.5h6" /></g></svg>
+                        
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
